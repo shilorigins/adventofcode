7711681b7bf7433db6bc8e754f9051030a21c034
Devan Agrawal <devagr@slac.stanford.edu>
2025-01-06 11:41:29 -0800

    [2024][20] take second pass at implementation

--- 20.py
+++ 20.py
@@ -3,13 +3,13 @@ import os.path
 
 
 class Path:
-    def __init__(self, start, cheats=None, points=None):
-        self.cheats = cheats.copy() if cheats else []
+    def __init__(self, start, points=None):
         self.points = points.copy() if points else set()
+        self.points.add(start)
         self.head = start
 
     def length(self):
-        return len(self.points) - 2 + len(self.cheats)
+        return len(self.points)
 
 
 def find_start(grid):
@@ -39,20 +39,21 @@ def part01(grid):
     q = [Path(start)]
     while len(q) > 0:
         active_path = q.pop()
-        for i, j in adjacent(active_path.head, grid):
-            if (i, j) not in active_path.points and (i, j) not in active_path.cheats:
-                value = grid[i][j]
-                new_path = Path((i, j), active_path.cheats, active_path.points)
-                if value == '.':
-                    new_path.points.add((i, j))
+        value = grid[active_path.head[0]][active_path.head[1]]
+        if value == 'E':
+            paths.append(active_path)
+        elif value == '#':
+            for point in teleport_points:
+                if point not in active_path.points and grid[point[0]][point[1]] != '#':
+                    new_path = Path(point, active_path.points)
                     q.append(new_path)
-                elif value == '#' and len(active_path.cheats) < 2:
-                    if len(active_path.cheats) == 0 or active_path.cheats[0] in adjacent(new_path.head, grid):
-                        new_path.cheats.append((i, j))
-                        q.append(new_path)
-                elif value == 'E':
-                    new_path.points.add((i, j))
-                    paths.append(new_path)
+        else:
+            adjacent_points = adjacent(active_path.head, grid)
+            teleport_points = {point for point in adjacent(first, grid)}
+            for point in adjacent_points:
+                if point not in active_path.points and grid[point[0]][point[1]] != '#':
+                    new_path = Path(point, active_path.points)
+                    q.append(new_path)
     return paths
 
 
@@ -66,18 +67,17 @@ if __name__ == "__main__":
     machines = []
     with open(filepath) as f:
         grid = f.read().splitlines()
-    unique_paths = part01(grid)
-    fastest_honest = min([path for path in unique_paths if len(path.cheats) == 0], key=lambda p: p.length())
-    honest_record = len(fastest_honest.points)
-    cheats = set()
-    paths = []
-    for path in unique_paths:
-        if path.points not in cheats:
-            cheats.add(frozenset(path.points))
-            paths.append(path)
-    #decent_cheats = [path for path in paths if len(path.points) < honest_record - 100]
-    decent_cheats = [path for path in paths if path.length() < honest_record]
-    deficits = [fastest_honest.length() - path.length() for path in decent_cheats]
+    paths = part01(grid)
+    honest = max(paths, key=lambda p: p.length())
+    #cheats = set()
+    #paths = []
+    #for path in paths:
+    #    if path.points not in cheats:
+    #        cheats.add(frozenset(path.points))
+    #        paths.append(path)
+    #decent_cheats = [path for path in paths if len(path.points) < honest.length() - 100]
+    decent_cheats = [path for path in paths if path.length() < honest.length()]
+    deficits = [honest.length() - path.length() for path in decent_cheats]
     with open("20.graphs", 'w') as f:
         from pprint import pprint
         for path in decent_cheats:
@@ -87,6 +87,6 @@ if __name__ == "__main__":
             for i, j in path.cheats:
                 image[i][j] = '@'
             image = ["".join(row) for row in image]
-            print(f"Path: {fastest_honest.length() - path.length()} picoseconds saved", file=f)
+            print(f"Path: {honest.length() - path.length()} picoseconds saved", file=f)
             pprint(image, stream=f)
         pprint({length: deficits.count(length) for length in deficits})
