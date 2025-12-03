b6ec650638e886f7384b66358f44974512241837
Devan Agrawal <devagr@slac.stanford.edu>
2025-12-03 14:19:30 -0800

    [2025][01] move day.py into 01/

---
+++ 2025/01/day.py
@@ -0,0 +1,36 @@
+#filepath = "01.test"
+filepath = "01.input"
+
+
+def part01(rotations):
+    password = 0
+    pointer = 50
+    for step in rotations:
+        pointer = (pointer + step) % 100
+        if pointer == 0:
+            password += 1
+    return password
+
+
+def part02(rotations):
+    password = 0
+    pointer = 50
+    for step in rotations:
+        print("Step: ", step)
+        old_pointer = pointer
+        pointer += step
+        password += abs(pointer) // 100 + ((old_pointer < 0) != (pointer < 0)) - (step < 0 and old_pointer == 0)
+        pointer %= 100
+        print("Pointer: ", pointer)
+        print("Password: ", password)
+        print("\n")
+    return password
+
+
+if __name__ == "__main__":
+    with open(filepath) as f:
+        rotations = [
+            int(rotation[1:]) if rotation.startswith('R') else -int(rotation[1:]) for rotation in f.read().split('\n')[:-1]
+        ]
+    print("Part 01: ", part01(rotations))
+    print("Part 02: ", part02(rotations))
