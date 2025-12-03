bc2cd1f87060d614fb71f0e7a18cf6e3556b9342
Devan Agrawal <devagr@slac.stanford.edu>
2025-12-03 14:21:03 -0800

    [2025][02] input parsing works

---
+++ 2025/02/day.py
@@ -0,0 +1,22 @@
+import re
+
+filepath = "02.test"
+#filepath = "02.input"
+
+
+def part01(ranges):
+    pass
+
+
+def part02(ranges):
+    pass
+
+
+if __name__ == "__main__":
+    with open(filepath) as f:
+        ranges = []
+        for line in f.read().split('\n')[:-1]:
+            m = re.match(r"(\d*)-(\d*)", line)
+            ranges.append((int((m.group(1))), int(m.group(2))))
+    print("Part 01: ", part01(ranges))
+    print("Part 02: ", part02(ranges))
