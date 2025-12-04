7c65362034a4d2cacfab5a0514d5e90be98ab6d0
Devan Agrawal <devagr@slac.stanford.edu>
2025-12-04 08:35:08 -0800

    [2025] [04] [Part 01] solved; answer is 1433

--- 2025/04/day.py
+++ 2025/04/day.py
@@ -1,9 +1,34 @@
+from pprint import pprint
+
 filepath = "test.txt"
 #filepath = "input.txt"
 
 
+def propogate(char, counts, row, column):
+    height = len(counts)
+    width = len(counts[0])
+    for i, j in [
+        (row-1, column-1),
+        (row, column-1),
+        (row+1, column-1),
+        (row-1, column),
+        (row+1, column),
+        (row-1, column+1),
+        (row, column+1),
+        (row+1, column+1),
+    ]:
+        if i >= 0 and i < width and j >= 0 and j < width:
+            if char == "@":
+                counts[j][i] += 1
+
+
 def part01(grid):
-    pass
+    counts = [[0 for _ in row] for row in grid]
+    for j in range(len(grid)):
+        for i in range(len(grid[0])):
+            if grid[j][i] == '@':
+                propogate(grid[j][i], counts, i, j)
+    return len([1 for j in range(len(counts)) for i in range(len(counts[0])) if counts[j][i] < 4 and grid[j][i] == "@"])
 
 
 def part02(grid):
