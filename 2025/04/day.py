9ee0ef2b4acbe9a0c632f56b8631738be6e6d84f
Devan Agrawal <devagr@slac.stanford.edu>
2025-12-04 08:36:48 -0800

    [2025] [04] further process input before both parts

--- 2025/04/day.py
+++ 2025/04/day.py
@@ -4,7 +4,7 @@ filepath = "test.txt"
 #filepath = "input.txt"
 
 
-def propogate(char, counts, row, column):
+def propogate(char, counts, row, column, increment=True):
     height = len(counts)
     width = len(counts[0])
     for i, j in [
@@ -19,24 +19,27 @@ def propogate(char, counts, row, column):
     ]:
         if i >= 0 and i < width and j >= 0 and j < width:
             if char == "@":
-                counts[j][i] += 1
+                if increment:
+                    counts[j][i] += 1
+                else:
+                    counts[j][i] -= 1
 
 
-def part01(grid):
-    counts = [[0 for _ in row] for row in grid]
-    for j in range(len(grid)):
-        for i in range(len(grid[0])):
-            if grid[j][i] == '@':
-                propogate(grid[j][i], counts, i, j)
+def part01(grid, counts):
     return len([1 for j in range(len(counts)) for i in range(len(counts[0])) if counts[j][i] < 4 and grid[j][i] == "@"])
 
 
-def part02(grid):
+def part02(grid, counts):
     pass
 
 
 if __name__ == "__main__":
     with open(filepath) as f:
         grid = [list(row) for row in f.read().split('\n')[:-1]]
-    print("Part 01: ", part01(grid))
-    print("Part 02: ", part02(grid))
+    counts = [[0 for _ in row] for row in grid]
+    for j in range(len(grid)):
+        for i in range(len(grid[0])):
+            if grid[j][i] == '@':
+                propogate(grid[j][i], counts, i, j)
+    print("Part 01: ", part01(grid, counts))
+    print("Part 02: ", part02(grid, counts))
