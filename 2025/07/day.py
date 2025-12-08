43a3767895246b68d6bb74f340931a22a528297a
Devan Agrawal <devagr@slac.stanford.edu>
2025-12-08 09:41:51 -0800

    [2025] [07] parsing works

---
+++ 2025/07/day.py
@@ -0,0 +1,19 @@
+from pprint import pprint
+
+filepath = "test.txt"
+#filepath = "input.txt"
+
+
+def part01(grid):
+    pass
+
+
+def part02(grid):
+    pass
+
+
+if __name__ == "__main__":
+    with open(filepath) as f:
+        grid = [list(row) for row in f.read().split('\n')[:-1]]
+    print("Part 01: ", part01(grid))
+    print("Part 02: ", part02(grid))
