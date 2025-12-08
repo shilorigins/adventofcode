11cf3d323e83f7f49b8f75bbd7d0d2165c4317aa
Devan Agrawal <devagr@slac.stanford.edu>
2025-12-08 09:52:27 -0800

    [2025] [07] [Part 01] solved; answer is 1687

--- 2025/07/day.py
+++ 2025/07/day.py
@@ -5,7 +5,22 @@ filepath = "test.txt"
 
 
 def part01(grid):
-    pass
+    beams = set()
+    beams.add(grid[0].index('S'))
+    count = 0
+    for row in grid[1:]:
+        split = set()
+        splitters = {i for i, elem in enumerate(row) if elem == '^'}
+        for splitter in splitters:
+            try:
+                beams.remove(splitter)
+                split.add(splitter - 1)
+                split.add(splitter + 1)
+                count += 1
+            except KeyError:
+                pass
+        beams |= split
+    return count
 
 
 def part02(grid):
