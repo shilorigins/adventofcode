4e61130641ceb9d13af0af5b9642df58de0d7e8a
Devan Agrawal <devagr@slac.stanford.edu>
2025-12-09 10:17:48 -0800

    [2025] [09] [Part 01] solved; answer is 4755429952

--- 2025/09/day.py
+++ 2025/09/day.py
@@ -4,8 +4,16 @@ filepath = "test.txt"
 #filepath = "input.txt"
 
 
+def area(p1, p2):
+    return abs(p1[0] - p2[0] + 1) * abs(p1[1] - p2[1] + 1)
+
+
 def part01(tiles):
-    pass
+    largest = 0
+    for tile in tiles:
+        for other in tiles - {tile}:
+            largest = max(largest, area(tile, other))
+    return largest
 
 
 def part02(tiles):
